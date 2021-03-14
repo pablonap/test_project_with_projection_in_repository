9338a1e0a9a0ee128bcd11b3b60bcf0c7c4943a6
chipo <pnapoli33@gmail.com>
2021-03-14 18:25:22 -0300

    Rename class

--- src/main/java/com/binary_winters/test_project/TestProjectApplication.java
+++ src/main/java/com/binary_winters/test_project/TestProjectApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmployeesTestingApplication {
+public class TestProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeesTestingApplication.class, args);
+		SpringApplication.run(TestProjectApplication.class, args);
 	}
 
 }
